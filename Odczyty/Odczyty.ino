d2ca6ea513af86660a098d9b15a5166aebf2936c
RobinsoneC12 <lukasznickzi@gmail.com>
2016-03-22 17:38:13 +0100

    Poprawiono odczyt temperatury

---
+++ Odczyty/Odczyty.ino
@@ -0,0 +1,17 @@
+const int pinTermometru = 0;
+void setup() {
+Serial.begin(9600);
+
+}
+
+float odczyt_temperatury(){
+float temperatura;
+temperatura = (analogRead(pinTermometru)/1024.0)*500;
+temperatura = temperatura - 277;
+return temperatura;
+}
+
+void loop() {
+Serial.println(odczyt_temperatury());
+delay(1000);
+}
