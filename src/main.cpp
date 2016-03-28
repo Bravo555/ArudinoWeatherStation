d642f833a51fee29bf5457b64d913c689ef8e005
Marcel Guzik <mguzik555@gmail.com>
2016-03-28 02:31:32 +0200

    Dodano odczyt z czujnika światła

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,26 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <BH1750.h>
 
 bool isRaining(const int sensorPin);
 float readTemperature(const int sensorPin);
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+BH1750 lightMeter;
 const int temperatureSensorPin = 0;
 const int rainSensorPin = 2;
 
 void setup() {
 	Serial.begin(9600);
 	lcd.begin(20, 4);
+	lightMeter.begin();
 }
 
 void loop() {
 	float temperature = readTemperature(temperatureSensorPin);
 	bool rainStatus = isRaining(rainSensorPin);
+	int lightLevel = lightMeter.readLightLevel();
 
 	lcd.home();
 
@@ -26,6 +30,11 @@ void loop() {
 	lcd.setCursor(0, 1);
 	lcd.print("Is raining: ");
 	lcd.print(rainStatus ? "yes" : "no");
+
+	lcd.setCursor(0, 2);
+	lcd.print("Light: ");
+	lcd.print(lightLevel);
+	lcd.print("lx");
 
 	Serial.println(temperature);
 	Serial.println(rainStatus);
