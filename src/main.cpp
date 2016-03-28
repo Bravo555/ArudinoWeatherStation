295a5280eb8064297219fd85280563a2b89284ab
Marcel Guzik <mguzik555@gmail.com>
2016-03-28 02:20:37 +0200

    Dodano wyświetlanie temperatury i statusu deszczu na LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 bool isRaining(const int sensorPin);
 float readTemperature(const int sensorPin);
 
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 const int temperatureSensorPin = 0;
 const int rainSensorPin = 2;
 
 void setup() {
 	Serial.begin(9600);
+	lcd.begin(20, 4);
 }
 
 void loop() {
-	Serial.println(readTemperature(temperatureSensorPin));
-	Serial.println(isRaining(rainSensorPin));
+	float temperature = readTemperature(temperatureSensorPin);
+	bool rainStatus = isRaining(rainSensorPin);
+
+	lcd.home();
+
+	lcd.print("Temperature: ");
+	lcd.print(temperature);
+
+	lcd.setCursor(0, 1);
+	lcd.print("Is raining: ");
+	lcd.print(rainStatus ? "yes" : "no");
+
+	Serial.println(temperature);
+	Serial.println(rainStatus);
 	delay(1000);
 }
 
