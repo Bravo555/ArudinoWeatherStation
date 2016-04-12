35af73d69b4dee2423d9429e7826f71a6f933e1e
RobinsoneC12 <lukasznickzi@gmail.com>
2016-04-12 17:47:26 +0200

    Zmieniono typ obsługi LCD na działającvy równolegle z resztą programu.

--- src/pages.hpp
+++ src/pages.hpp
@@ -1,53 +1,43 @@
 #include "sensorData.hpp"
 
 const int rightButtonPin=1, leftButtonPin=2;
-
-void screenPrinting()
-{
-	short int screenPage = 1;
-	while(true)
-	{
-		if(digitalRead(rightButtonPin)==LOW)
-			screenPage++;
-		if(digitalRead(leftButtonPin)==LOW)
-			screenPage--;
-		if(screenPage==0)
-			break;
-		switch(screenPage)
-		{
-<<<<<<< HEAD
-			case 1:
-
-			break;
-
-			case 2:
-
-			break;
-=======
-
->>>>>>> origin/master
-		}
-	}
-}
+short int screenPage = 0;
 
 void overviewPage()
 {
 	lcd.home();
 
-	lcd.print("Temperature: ");
+	lcd.print("Temperatura: ");
 	lcd.print(temperature);
 
 	lcd.setCursor(0, 1);
-	lcd.print("Is raining: ");
-	lcd.print(rainStatus ? "yes" : "no");
+	lcd.print("Czy pada: ");
+	lcd.print(rainStatus ? "tak" : "nie");
 
 	lcd.setCursor(0, 2);
-	lcd.print("Light: ");
+	lcd.print("Swiatlo: ");
 	lcd.print(lightLevel);
 	lcd.print("lx");
 
 	lcd.setCursor(0, 3);
-	lcd.print("Pressure: ");
+	lcd.print("Cisnienie: ");
 	lcd.print(pressure);
 	lcd.print("kPa");
+}
+
+void screenPrinting()
+{
+		if(digitalRead(rightButtonPin)==LOW)
+			screenPage++;
+		if(digitalRead(leftButtonPin)==LOW)
+			screenPage--;
+		switch(screenPage)
+		{
+			case 0:
+				overviewPage();
+			break;
+			case 2:
+
+			break;
+		}
 }
